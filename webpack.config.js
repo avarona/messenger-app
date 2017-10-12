aad607711b83627b5939591f308fb2a0de8729e4
avarona <avarona10@gmail.com>
2017-10-12 13:20:01 -0400

    chore: updated babel presets

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         options: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'env']
         }
       }, {
         test: /\.(scss|sass)$/,
